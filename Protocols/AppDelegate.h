8519eb768386dcfc6487f41bc7971ea34e30b368
EnzoFurmenti <ducheenzo@mail.ru>
2016-07-24 10:06:59 +0300

    Initial Commit

---
+++ Protocols/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Protocols
+//
+//  Created by EnzoF on 24.07.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
